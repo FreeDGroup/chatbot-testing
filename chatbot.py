3bd4b8ac1a167bfe40ca859fcb13cc6cf89c4718
xncbf <xncbf12@gmail.com>
2018-10-30 12:29:49 +0900

    .

--- chatbot.py
+++ chatbot.py
@@ -51,7 +51,7 @@ class FBChatbotTest:
             else:
                 filename = 'screenshots/' + str(datetime.now()) + '.png'
                 self.driver.save_screenshot(filename)
-                s3_transfer.upload_file('./%s' % filename, 'flanb-data', 'test.png')
+                s3_transfer.upload_file('./%s' % filename, 'flanb-data', '/travis/%s' % filename)
                 self.driver.quit()
         return False
 
